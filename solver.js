32d0359a75cf68652b5a11eb166d4359ef67680d
Joshua Holbrook <josh.holbrook@gmail.com>
2013-03-16 22:37:16 -0800

    outputs of noweb

---
+++ solver.js
@@ -0,0 +1,117 @@
+var num = require('numeric');
+
+function _solve(prices, bases, risks, budget) {
+
+  var n = prices.length;
+
+  function p(buys) {
+    //
+    // Note that in the woven code, prices, bases, buys, risks and budget
+    // will all be defined.
+    //
+    function dLdci(i) {
+      return prices[i] - bases[i] * Math.pow(buys[i], risks[i]);
+    }
+    function dLdl() {
+      var sum = 0;
+      for (i = 0; i < n; i++) {
+        sum += prices[i] * buys[i];
+      }
+      return sum - budget;
+    }
+    function d2Ldci2(i) {
+      return - bases[i] * risks[i] * Math.pow(buys[i], risks[i] - 1);
+    }
+    function d2Ldcidl() {
+      var sum = 0;
+      for (i = 0; i < n; i++) {
+        sum += prices[i];
+      }
+      return sum;
+    }
+
+    var hf = Array.apply(null, Array(n))
+      .map(function (_, i) {
+        return Array.apply(null, Array(n))
+          .map(function (_, j) {
+            if (i == j) {
+              return d2Ldci2(i);
+            }
+            else {
+              return 0;
+            }
+          })
+          .concat(d2Ldcidl())
+        ;
+      })
+      .concat([
+        Array.apply(null, Array(prices.length))
+          .map(function (_, i) {
+            return d2Ldcidl(i);
+          })
+          .concat(0)
+      ])
+    ;
+
+    var del = Array.apply(null, Array(n))
+      .map(function (_, i) {
+        return dLdci(i);
+      })
+      .concat(dLdl())
+    ;
+
+    return num.solve(hf, del);
+  }
+
+  var buys = Array.apply(null, Array(n + 1))
+        .map(function () { return 100; }),
+      delta = buys.map(function () { return 100; }),
+      gamma = 1, // Smaller step sizes may perform better, this is empirically adjusted
+      epsilon = 0.01, // Use this to adjust precision
+      next;
+
+  while (num.norm2(delta) > epsilon) {
+    next = num.sub(buys, p(buys));
+    delta = buys - next;
+    buys = next;
+  }
+
+  buys.pop();
+
+  return buys.map(function (b) {
+    return Math.max(0, Math.floor(b));
+  });
+}
+
+module.exports = function (params, budget) {
+  var keys = Object.keys(params),
+      prices = [],
+      bases = [],
+      risks = [],
+      buys = {};
+
+  keys.forEach(function (k) {
+    prices.push(params[k].price);
+    bases.push(params[k].base);
+    risks.push(params[k].risk);
+  });
+
+  _solve(prices, bases, risks, budget).forEach(function (b, i) {
+    buys[keys[i]] = b;
+  });
+
+  return buys;
+};
+
+\section{caveats}
+
+Recall that we assumed that this function was well-behaved enough to avoid
+negative values. That means that if the optimum is near zero for some
+products, or if our objective funtion is not ``well-behaved'', that our method
+will probably not perform very well.
+
+Also note that, in real life, when consumers have a choice between two
+similar products which require a base investment, that there will be a cost
+associated with using more than one product (example: using AWS and RackSpace
+instead of just AWS or just RackSpace). This model does not take that into
+consideration at all.
